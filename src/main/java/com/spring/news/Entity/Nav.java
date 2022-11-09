aa9d177913317b1bbd351db70f16e0f13a2e09eb
cuongnguyen <cuongnqph06515@gmail.com>
2022-11-09 23:31:22 +0700

    sidebar

---
+++ src/main/java/com/spring/news/Entity/Nav.java
@@ -0,0 +1,59 @@
+package com.javacore.algorithm;
+
+import java.util.ArrayList;
+
+public class Nav {
+    int id;
+    String name;
+    int parentId;
+    ArrayList<Nav> child;
+
+    public Nav(){
+    }
+
+    public Nav(int id, String name, int parentId) {
+        this.id = id;
+        this.name = name;
+        this.parentId = parentId;
+    }
+
+    public Nav(int id, String name, int parentId, ArrayList<Nav> child) {
+        this.id = id;
+        this.name = name;
+        this.parentId = parentId;
+        this.child = child;
+    }
+
+    public ArrayList<Nav> getChild() {
+        return child;
+    }
+
+    public void setChild(ArrayList<Nav> child) {
+        this.child = child;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+}
