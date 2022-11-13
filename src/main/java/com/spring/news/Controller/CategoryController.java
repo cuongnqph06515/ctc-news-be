6fad72e8ffd0d4caad8fef6df4f6bac3b71e4b19
cuongnguyen <cuongnqph06515@gmail.com>
2022-11-13 16:22:22 +0700

    d

--- src/main/java/com/spring/news/Controller/CategoryController.java
+++ src/main/java/com/spring/news/Controller/CategoryController.java
@@ -24,7 +24,7 @@ public class CategoryController {
         List<Category> categoryList = categoryService.getAllCategory();
         apiResponse.setData(categoryList);
         System.out.println("1");
-        System.out.println("mater update");
+        System.out.println("2");
         System.out.println("cuongnguyen1 update");
         return new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.OK);
     }
